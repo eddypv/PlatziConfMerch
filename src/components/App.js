a04a42c745ce9f2e2725cbeae87a9dc6468cfd24
Eddy <eddypv0@gmail.com>
2021-05-27 10:56:27 -0400

    Include in rules of webpack Css Extract plugin and css-loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../styles/components/app.css'
 const App = () => {
     return (<h1>Hola Mundo</h1>)
 } 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin  = require('html-webpack-plugin')
-
+const MiniCssExtractPluguin = require('mini-css-extract-plugin')
 module.exports ={
     entry:'./src/index.js', // punto de entrada
     output:{
@@ -24,13 +24,25 @@ module.exports ={
                 use:{
                     loader:'html-loader'
                 }
-            }
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    {
+                        loader:MiniCssExtractPluguin.loader
+                    },
+                    "css-loader"
+                ]
+            },
         ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             template:'./public/index.html',
             filename:'./index.html'
+        }),
+        new MiniCssExtractPluguin({
+            filename:'assets/[name].css'
         })
     ],
     devServer:{
